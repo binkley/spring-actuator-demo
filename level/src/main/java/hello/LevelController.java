92a42ee0357a6efcff10827dd25c9413fe4634bf
Brian Oxley <boxley@thoughtworks.com>
2015-04-25 08:08:03 -0500

    Add level module for HATEOAS.

---
+++ level/src/main/java/hello/LevelController.java
@@ -0,0 +1,42 @@
+package hello;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static java.lang.String.format;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class LevelController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/remote-hello", method = GET)
+    public Greeting remoteHello(
+            @RequestParam("name") final Optional<String> name) {
+        // TODO: How to do with with @Valid and ilk?
+        return new Greeting(counter.incrementAndGet(),
+                format("Hello, %s!", name.
+                        filter(s -> !s.isEmpty()).
+                        orElseThrow(LevelController::badName)));
+    }
+
+    @ExceptionHandler
+    public void badArgs(final IllegalArgumentException e,
+            final HttpServletResponse response)
+            throws IOException {
+        response.sendError(BAD_REQUEST.value(), e.getMessage());
+    }
+
+    private static IllegalArgumentException badName() {
+        return new IllegalArgumentException(
+                "Required String parameter 'name' is missing or empty");
+    }
+}
---
+++ level/src/main/java/hello/LevelMain.java
@@ -0,0 +1,11 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelMain {
+    public static void main(final String... args) {
+        SpringApplication.run(LevelMain.class, args);
+    }
+}
